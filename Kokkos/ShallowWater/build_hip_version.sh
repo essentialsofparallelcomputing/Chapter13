d692c4a85f588519eb94c9f803e66415170c7539
Bob Robey <brobey@lanl.gov>
2022-05-31 20:23:54 -0600

    Adding hip version

---
+++ Kokkos/ShallowWater/build_hip_version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export OMP_PROC_BIND=true
+
+mkdir hip_build; cd hip_build
+cmake -DKokkos_ENABLE_HIP=ON \
+      -DKokkos_ARCH_ZEN=ON \
+      -DKokkos_ARCH_VEGA906=ON \
+      -DCMAKE_CXX_COMPILER=hipcc \
+      ..
+
+make -j 8
+
+./ShallowWater
+./ShallowWater_par1
+./ShallowWater_par2
+./ShallowWater_par3
+./ShallowWater_par4
